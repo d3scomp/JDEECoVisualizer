9934193273995d23234b522c6cc87dcee05216e9
Ilias Gerostathopoulos <iliasger@gmail.com>
2015-10-26 22:36:39 +0100

    Updated tests, now generated XML network files validated with DTD

--- test/cz/cuni/mff/d3s/jdeeco/visualizer/network/NetworkTest.java
+++ test/cz/cuni/mff/d3s/jdeeco/visualizer/network/NetworkTest.java
@@ -1,124 +1,105 @@
 package cz.cuni.mff.d3s.jdeeco.visualizer.network;
+
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.junit.Test;
-
-import junitx.framework.FileAssert;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 public class NetworkTest {
 
-	protected File tempFile;
 	protected Network network;
-	
-	@Before
-	public void setUp() throws Exception {
-		tempFile = Files.createTempFile(null, ".xml").toFile();
-	}
+	private static final String PATH_PREFIX = "target" + File.separator + "test-classes" + File.separator;
 
-	@After
-	public void tearDown() throws IOException {
-		Files.deleteIfExists(tempFile.toPath());
-	}
-	
-	/*
-	 * Acceptance tests. 
-	 * The produced XML-serialized models (saved in a temporary file) 
-	 * are compared to their pre-generated expected outputs.
-	 */
-	
-	@Test 
-	public void testSmallSquareNetworkCreation() throws IOException {
+	@Test
+	public void testSmallSquareNetworkCreation() throws IOException, ParserConfigurationException {
 		network = new Network();
 		int width = 5;
 		int height = 5;
-		
+
 		Node.resetIdReference();
 		createAndAttachNodes(width, height);
 		createAndAttachLinks(width, height);
 
-//		writeToFile("experiment.xml");
-		writeToTempFile();
-		File expected = getExpectedFile("smallSquareNetwork.xml");
-		FileAssert.assertEquals(expected, tempFile);
+		String pathToCreatedFile = writeToFile("smallSquareNetwork.xml");
+		assertTrue(validateWithDTD(pathToCreatedFile));
 	}
-	
-//	@Test @Ignore
-//	public void testMediumSquareNetworkCreation() throws IOException, InterruptedException {
-//		network = new Network();
-//		int width = 10;
-//		int height = 10;
-//		
-//		Node.resetIdReference();
-//		createAndAttachNodes(width, height);
-//		createAndAttachLinks(width, height);
-//
-////		writeToFile("mediumSquareNetwork.xml");
-//		writeToTempFile();
-//		File expected = getExpectedFile("mediumSquareNetwork.xml");
-//		Thread.sleep(1000);
-//
-//		FileAssert.assertEquals(expected, tempFile);
-//	}
-//
-//	@Test @Ignore 
-//	public void testRectangleNetworkCreation() throws IOException {
-//		network = new Network();
-//		int width = 5;
-//		int height = 3;
-//		
-//		Node.resetIdReference();
-//		createAndAttachNodes(width, height);
-//		createAndAttachLinks(width, height);
-//
-////		writeToFile("mediumRectangleNetwork.xml");
-//		writeToTempFile();
-//		File expected = getExpectedFile("mediumRectangleNetwork.xml");
-//		FileAssert.assertEquals(expected, tempFile);
-//	}
-	
+
+	@Test
+	public void testMediumSquareNetworkCreation() throws IOException, InterruptedException, ParserConfigurationException {
+		network = new Network();
+		int width = 10;
+		int height = 10;
+
+		Node.resetIdReference();
+		createAndAttachNodes(width, height);
+		createAndAttachLinks(width, height);
+
+		String pathToCreatedFile = writeToFile("mediumSquareNetwork.xml");
+		assertTrue(validateWithDTD(pathToCreatedFile));
+	}
+
+	@Test
+	public void testRectangleNetworkCreation() throws IOException, ParserConfigurationException {
+		network = new Network();
+		int width = 10;
+		int height = 5;
+
+		Node.resetIdReference();
+		createAndAttachNodes(width, height);
+		createAndAttachLinks(width, height);
+
+		String pathToCreatedFile = writeToFile("mediumRectangleNetwork.xml");
+		assertTrue(validateWithDTD(pathToCreatedFile));
+	}
+
 	protected void createAndAttachNodes(int width, int height) {
-		for (int i=0; i<width; i++) {
-			for (int j=0; j<height; j++) {
-				Node n = new Node(i,j);
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				Node n = new Node(i, j);
 				network.addNode(n);
 			}
 		}
 	}
-	
+
 	protected void createAndAttachLinks(int width, int height) {
-		for (int id=0; id<width*height-1; id++) {
+		for (int id = 0; id < width * height - 1; id++) {
 			// add vertical links
 			if (id % height != height - 1) {
 				Node upper = getNode(id);
-				Node lower = getNode(id+1);
-				network.addLink(new Link(upper,lower));
+				Node lower = getNode(id + 1);
+				network.addLink(new Link(upper, lower));
 				// add diagonal links
-				if (id < width*height - height) {
-					// add upper left to lower right links	
+				if (id < width * height - height) {
+					// add upper left to lower right links
 					Node upperLeft = getNode(id);
-					Node lowerRight = getNode(id+height+1);
-					network.addLink(new Link(upperLeft,lowerRight));
+					Node lowerRight = getNode(id + height + 1);
+					network.addLink(new Link(upperLeft, lowerRight));
 					// add lower left to upper right links
-					Node lowerLeft = getNode(id+1);
-					Node upperRight = getNode(id+height);
-					network.addLink(new Link(lowerLeft,upperRight)); 
+					Node lowerLeft = getNode(id + 1);
+					Node upperRight = getNode(id + height);
+					network.addLink(new Link(lowerLeft, upperRight));
 				}
 			}
 			// add horizontal links
-			if (id < width*height - height) {
+			if (id < width * height - height) {
 				Node left = getNode(id);
-				Node right = getNode(id+height);
-				network.addLink(new Link(left,right)); 
+				Node right = getNode(id + height);
+				network.addLink(new Link(left, right));
 			}
-		}		
+		}
 	}
 
 	private Node getNode(int id) {
@@ -129,34 +110,53 @@ public class NetworkTest {
 		}
 		return null;
 	}
-	
-	private void writeToTempFile() {
+
+	private String writeToFile(String name) {
 		PrintWriter writer = null;
+		String path = PATH_PREFIX  + name;
 		try {
-			writer = new PrintWriter(tempFile.getPath(), "UTF-8");
+			writer = new PrintWriter(path, "UTF-8");
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		writer.println(network.toString());
 		writer.close();
+		return path;
+
 	}
-	
-	private void writeToFile(String name) {
-		PrintWriter writer = null;
+
+	private static boolean validateWithDTD(String xml) throws ParserConfigurationException, IOException {
 		try {
-			writer = new PrintWriter(name, "UTF-8");
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setValidating(true);
+			factory.setNamespaceAware(true);
+
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			builder.setErrorHandler(new ErrorHandler() {
+				public void warning(SAXParseException e) throws SAXException {
+					System.out.println("WARNING : " + e.getMessage()); // do
+				}
+
+				public void error(SAXParseException e) throws SAXException {
+					System.out.println("ERROR : " + e.getMessage());
+					throw e;
+				}
+
+				public void fatalError(SAXParseException e) throws SAXException {
+					System.out.println("FATAL : " + e.getMessage());
+					throw e;
+				}
+			});
+			builder.parse(new InputSource(xml));
+			return true;
+		} catch (ParserConfigurationException pce) {
+			throw pce;
+		} catch (IOException io) {
+			throw io;
+		} catch (SAXException se) {
+			return false;
 		}
-		writer.println(network.toString());
-		writer.close();
 	}
-	
-	private File getExpectedFile(String name) {
-		String outF = "target" + File.separator + "test-classes" + File.separator;
-		String path = outF + name;
-		return new File(path);
-	}
+
 }
