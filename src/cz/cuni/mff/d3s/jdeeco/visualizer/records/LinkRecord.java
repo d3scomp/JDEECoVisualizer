c7f4c457ffcad1536d232d15fecbf184e6e4fdb7
Dominik Skoda <skoda@d3s.mff.cuni.cz>
2015-11-23 13:58:30 +0100

    LinkRecord specifies a vehicle as well now.

--- src/cz/cuni/mff/d3s/jdeeco/visualizer/records/LinkRecord.java
+++ src/cz/cuni/mff/d3s/jdeeco/visualizer/records/LinkRecord.java
@@ -1,22 +1,15 @@
 package cz.cuni.mff.d3s.jdeeco.visualizer.records;
 
-import java.util.HashMap;
-
-import cz.cuni.mff.d3s.deeco.runtimelog.RuntimeLogRecord;
 import cz.cuni.mff.d3s.jdeeco.visualizer.network.Link;
 
-public abstract class LinkRecord extends RuntimeLogRecord {
+public abstract class LinkRecord extends VehicleRecord {
 
 	public LinkRecord(String id) {
-		super(id, new HashMap<String, Object>());
+		super(id);
 	}
 	
 	public void setLink(Link link){
 		recordValues.put("link", link.getId());
-	}
-	
-	public void setPerson(String person){
-		recordValues.put("person", person);
 	}
 
 }
